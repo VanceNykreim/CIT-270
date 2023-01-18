39935c77ea396db60d0569a07157ee952568c487
VanceNykreim <vanceyadi@msn.com>
2023-01-18 13:39:10 -0700

    installed body parser and nodemon, edited server.js and createlogin.cmd

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const app = express();
 
 const port = 3000;
 
+const bodyParser = require ('body-parser');
+
+app.use(bodyParser.json());//application middleware, looks for incoming data
+
 app.get("/", (req,res) => {
     res.send("Hello Vance");
+});
+
+app.post('/login', (req, res) => {
+    const loginUser = req.body.userName;
+    console.log('Login username:'+loginUser);
+    res.send('Hello '+loginUser);
 });
 
 app.listen(port, () => {
